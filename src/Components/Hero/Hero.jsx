9287f04311aaa00f9c6b3e6a4048965232a48ced
Giancarlo49 <giancarloscalisi2@gmail.com>
2024-07-16 18:10:36 +0200

    startpage angepasst

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className='hero'>
           <img src={profile_img} alt="" />
           <h1><span>I`m Carlo,</span> web developer based in Germany.</h1>
-          <p>I am a web developer from Hamburg, Germany with 2 years of experience</p>
+          <p>I am a web developer from Hamburg, Germany with 2 years of experience with React JavaScript, Python, Flutter</p>
           <div className="hero-action">
               <div className="hero-connection">Connect With Me</div>
               <div className="hero-resume">My Resume</div>
