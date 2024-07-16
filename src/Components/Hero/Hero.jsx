00e5c9fa6ad9c094adc1af02b1e55c6ffb3d0cc2
Giancarlo49 <giancarloscalisi2@gmail.com>
2024-07-16 16:53:10 +0200

    startpage geruest

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import 'react'
 import './Hero.css'
-import profile_img from '../../assets/profile_img.png'
+import profile_img from '../../assets/profile_img.jpg'
 
 const Hero = () => {
   return (
       <div className='hero'>
           <img src={profile_img} alt="" />
-          <h1>I`m Carlo, web developer based in Germany.</h1>
+          <h1><span>I`m Carlo,</span> web developer based in Germany.</h1>
           <p>I am a web developer from Hamburg, Germany with 2 years of experience</p>
           <div className="hero-action">
               <div className="hero-connection">Connect With Me</div>
