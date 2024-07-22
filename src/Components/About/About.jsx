da15016ba81a4932938598e2e6564e0092726d58
Giancarlo49 <giancarloscalisi2@gmail.com>
2024-07-22 15:49:40 +0200

    about page fertig gestellt und startpage angepasst

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -17,13 +17,14 @@ const About = () => {
               <div className="about-right">
                   <div className="about-para">
                       <p>I am a Software Developer with more than two years of experience</p>
-                      <p>My passion for Developing ist ReactJS Flutter and Python</p>
+                      <p>My passion for Developing is ReactJS Flutter and Python</p>
                   </div>
                   <div className="about-skills">
                       <div className="about-skill"><p>HTML & CSS</p><hr style={{width: "60%"}}/></div>
-                      <div className="about-skill"><p>React JS & JavaScript</p><hr style={{width: "70%"}}/></div>
+                      <div className="about-skill"><p>React JS</p><hr style={{ width: "70%" }} /></div>
+                      <div className="about-skill"><p>JavaScript</p><hr style={{width: "60%"}}/></div>
                       <div className="about-skill"><p>Flutter</p><hr style={{width: "50%"}}/></div>
-                      <div className="about-skill"><p>Python</p><hr style={{width: "60%"}}/></div>
+                      <div className="about-skill"><p>Python</p><hr style={{width: "55%"}}/></div>
                   </div>
               </div>
           </div>
@@ -31,6 +32,16 @@ const About = () => {
               <div className="about-achievement">
                   <h1>2+</h1>
                   <p>YEARS OF EXPERIENCE</p>
+              </div>
+              <hr />
+              <div className="about-achievement">
+                  <h1>15+</h1>
+                  <p>PROJECTS COMPLETED</p>
+              </div>
+              <hr />
+              <div className="about-achievement">
+                  <h1>2+</h1>
+                  <p>HAPPY CLIENTS</p>
               </div>
           </div>
     </div>
