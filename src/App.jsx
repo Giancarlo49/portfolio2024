8496da0a369662c7b9a14c7ac98b3b3ba1c2e48b
Giancarlo49 <giancarloscalisi2@gmail.com>
2024-08-04 12:05:55 +0200

    Footer fertig gestellt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import  'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About/About'
-import Services from './Components/Services/Services'
-import MyWork from './Components/MyWork/MyWork'
-import Contact from './Components/Contact/Contact'
+import "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import MyWork from "./Components/MyWork/MyWork";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
@@ -14,10 +15,10 @@ const App = () => {
       <About />
       <Services />
       <MyWork />
-      <Contact/>
-      
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import "react";
+import "./Footer.css";
+import footer_logo from "../../assets/footer_logo.svg";
+import user_icon from "../../assets/user_icon.svg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          {/* <img src={footer_logo} alt="" /> */}
+          <h2 className="meinName">Carlo</h2> {/* ersetz das img_logo */}
+          <p>
+            I am a frontend developer from Germany with more than 2 years of
+            experience
+          </p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="email" placeholder="Enter Your Email" />
+          </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          © 2024 Giancarlo Scalisi. All rights reserved.
+        </p>
+        <div className="footer-bottom-right">
+          <p>Term Of Services</p>
+          <p>Privacy Policy</p>
+          <p>Connect With Me</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
